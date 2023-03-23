b90021f2cd107d767482f809ce3edec7f71f4efa
Ayobami Alaran A <ayobamidele006@gmail.com>
2023-03-23 21:30:51 +0100

    [Workflow/fix]:
    fixed workflow error in settings.py

--- peers/settings.py
+++ peers/settings.py
@@ -123,8 +123,8 @@ DATABASES = {
 
 
 # Sqlite
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR / 'db.sqlite3',
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 Ps = 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
